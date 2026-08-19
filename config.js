414feb490d7ef9ce4fd35adca70fb9c37e7f7d91
laofe2025 <laofe2025@gmail.com>
2026-08-19 15:46:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
  * 2) ADMIN_PIN = ຕ້ອງກົງກັບ ADMIN_PIN ໃນ Code.gs
  **********************************************************************/
 window.LAOFE_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbwMDcQgy8n6fyjMpl_OYoYhv278doF0T3o24_Uoc5oQPGDdVCmFPnSdHUKJqNdz4tyU/exec',   // ← ວາງລິ້ງທີ່ໄດ້ຈາກ Apps Script
+  API_URL: 'https://script.google.com/macros/s/AKfycbx1pHVpeQKe6C3oijsmdolBUNC6vGxDbxj9AC8XrwvUil7eBohuyiNVrD_1sBkuqkhgBA/exec',   // ← ວາງລິ້ງທີ່ໄດ້ຈາກ Apps Script
   ADMIN_PIN: '1234',                          // ← ໃຫ້ກົງກັບ Code.gs
   SHOP_NAME: 'LAOFE',
   SHOP_SUB: 'CAFE & BEER',
